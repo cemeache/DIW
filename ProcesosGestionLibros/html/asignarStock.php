f59b34e7b5a221eac75db04f1e97c4715d70fe90
celia.evg <2DAW01@esvirgua.es>
2024-11-20 13:50:15 +0100

    Stock

--- ProcesosGestionLibros/html/asignarStock.php
+++ ProcesosGestionLibros/html/asignarStock.php
@@ -6,10 +6,15 @@
     if(isset($_POST["seleccionados"]))
         print_r($_POST["seleccionados"]);
 
+    //Mostrar Stock de Libro con X ISBN
     $consulta = "SELECT stock FROM libro WHERE isbn = '9781122334455'";
     $resultado = $mysqli->query($consulta);
 
     $fila = $resultado->fetch_assoc();
 
     echo "<br> Stock: ".$fila["stock"];
+
+    for ($i=0; $i < count($_POST["seleccionados"]); $i++) { 
+        
+    }
 ?>
