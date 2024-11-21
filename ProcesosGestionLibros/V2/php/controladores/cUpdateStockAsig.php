ad4e9e4ac6a2edab0d2a28259f4203e6ca876214
Merroji <156344132+merroji@users.noreply.github.com>
2024-11-21 18:34:05 +0100

    PRYCT | GestionLibros | cUpdateStockAsig -> INI v2

---
+++ ProcesosGestionLibros/V2/php/controladores/cUpdateStockAsig.php
@@ -0,0 +1,12 @@
+<?php
+    require_once ('../modelos/mUpdateStockAsig.php');
+
+    class CUpdateStockAsig{
+
+        public function __construct(){
+
+        }
+
+        
+    }
+?>
---
+++ ProcesosGestionLibros/V2/php/modelos/mUpdateStockAsig.php
@@ -0,0 +1,12 @@
+<?php
+    require_once('../config/conectar.php');
+
+    class MUpdateStockAsig extends Conectar{
+
+        public function __construct(){
+
+        }
+        
+    }
+
+?>
